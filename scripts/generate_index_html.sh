3910cc087da396bebd5f85d4fa53c63b12770caa
Kenny Ballou <kballou@devnulllabs.io>
2020-11-26 00:33:35 -0700

    add banner image
    Various Small adjustments to CSS to accommodate.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- scripts/generate_index_html.sh
+++ scripts/generate_index_html.sh
@@ -8,6 +8,8 @@ source ${PROJ_ROOT}/scripts/site-templates.sh
 cat "${HTML_HEADER_FILE}"
 echo "<body>"
 cat "${HTML_SUB_HEADER_FILE}"
+echo "<div id=\"content\">"
 cat ${INPUT_FILES} | sort -r -n -k1 -k2 -k3 | awk -F'	' '{print $4}'
+echo "</div>"
 echo "</body>"
 cat "${HTML_FOOTER_FILE}"
